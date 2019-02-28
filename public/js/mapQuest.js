cd04be6ab0227f11dfadf82091845ec87e18ca57
Lawrence Ngo <l9ngo@ucsd.edu>
2019-02-28 10:44:51 -0800

    thirdPartyAPI lab

--- public/js/mapQuest.js
+++ public/js/mapQuest.js
@@ -1,3 +1,13 @@
 function initMap() {
 	// add your code here
+	L.mapquest.key = 'VWwUgHWLgMm6ZOgOL4SxNSakPICPyqCz'
+
+	var map = L.mapquest.map('map',{
+		center: [32.88, -117.236],
+		layers: L.mapquest.tileLayer('map'),
+		zoom: 14,
+		zoomControl: false
+	});
+
+	L.marker([32.88, -117.236]).addTo(map);
 }
